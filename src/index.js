d512ba09d88d148fcf543c76d34739f0ecd6a49a
Geir Sande <geir.sande@luukku.com>
2022-06-06 13:05:13 +0300

    using React Router (with downgraded React)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from './components/App';
+import Jokes from './components/Jokes';
+import './index.css';
+
+const root = document.getElementById("root");
+ReactDOM.render(
+  <Router history={createBrowserHistory()}>
+    <Switch>
+      <Route exact={true} path="/" component={App} />
+      <Route path="/jokes" component={Jokes} />
+    </Switch>
+  </Router>
+, root);
