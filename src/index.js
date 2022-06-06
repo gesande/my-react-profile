c67f7f9f651483c7c7b6415cd76e4b13bf924747
Geir Sande <geir.sande@luukku.com>
2022-06-06 14:08:05 +0300

    Using High Order Component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Switch, Route } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-import App from './components/App';
-import Jokes from './components/Jokes';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import App from "./components/App";
+import Jokes from "./components/Jokes";
+import Header from "./components/Header";
+import "./index.css";
 
 const root = document.getElementById("root");
 ReactDOM.render(
   <Router history={createBrowserHistory()}>
     <Switch>
-      <Route exact={true} path="/" component={App} />
-      <Route path="/jokes" component={Jokes} />
+      <Route
+        exact={true}
+        path="/"
+        render={() => (
+          <Header>
+            <App />
+          </Header>
+        )}
+      />
+      <Route
+        path="/jokes"
+        render={() => (
+          <Header>
+            <Jokes />
+          </Header>
+        )}
+      />
     </Switch>
-  </Router>
-, root);
+  </Router>,
+  root
+);
